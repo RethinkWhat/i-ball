9a0de9a9079dfd65916acb034721ac3576a96c94
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2024-03-25 13:31:24 +0800

    Added model templates

---
+++ src/client/fan/model/FanApplicationModel.java
@@ -0,0 +1,8 @@
+package client.fan.model;
+
+/**
+ * The FanApplicationView provides methods to invoke SQL connection, SQL statements to provide
+ * fan user information.
+ */
+public class FanApplicationModel {
+}
---
+++ src/client/idol/model/IdolApplicationModel.java
@@ -0,0 +1,8 @@
+package client.idol.model;
+
+/**
+ * The IdolApplicationView provides methods to invoke SQL connection, SQL statements to provide
+ * idol user information.
+ */
+public class IdolApplicationModel {
+}
