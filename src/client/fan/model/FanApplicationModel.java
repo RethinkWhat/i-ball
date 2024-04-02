77facb49713c8e681e319db2e303df16fc724215
RAMON EMMIEL JASMIN <2230748@slu.edu.ph>
2024-04-02 20:35:07 +0800

    Added initial code for FanApplicationModel

--- src/client/fan/model/FanApplicationModel.java
+++ src/client/fan/model/FanApplicationModel.java
@@ -1,8 +1,22 @@
 package client.fan.model;
 
+import shared.Idol;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * The FanApplicationView provides methods to invoke SQL connection, SQL statements to provide
  * fan user information.
  */
 public class FanApplicationModel {
+
+    public List<Idol> getAllIdols(ResultSet idols){
+        List<Idol> idolList = new ArrayList<>();
+
+
+
+        return idolList;
+    }
 }
