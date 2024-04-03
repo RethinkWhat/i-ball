e5140be30395597821f5327b8d649d54b2b38ed5
Rithik Tank <2233293@slu.edu.ph>
2024-04-03 22:25:01 +0800

    user login

--- src/shared/res/DataPB.java
+++ src/shared/res/DataPB.java
@@ -57,35 +57,36 @@ public class DataPB {
      */
     public static Object validateUser(String username, String password) throws SQLException{
         //TODO : Access schema and check whether user exists
+        DataPB.setCon();
 
-        /*
-        String query = "SELECT * FROM ? WHERE username=? and password=?";
+        String tableName = "User";
+        String query = "SELECT userID, username, GCashNumber, profilePictureAddress FROM " + tableName + " WHERE username=? and password=?";
+
         PreparedStatement stmt = con.prepareStatement(query);
 
-        stmt.setString(1, "User");
-        stmt.setString(2, username);
-        stmt.setString(3, password);
-        ResultSet account = stmt.executeQuery(query);
+        stmt.setString(1, username);
+        stmt.setString(2, password);
+        ResultSet account = stmt.executeQuery();
+
         if (account.next()) {
             int userID = account.getInt("userID");
             String email = account.getString("username");
             String gCashNumber = account.getString("GCashNumber");
-            String ppAddress = account.getString("ppAddress");
+            String ppAddress = account.getString("profilePictureAddress");
             return new User(userID, username, email, password, gCashNumber, ppAddress);
-        } else {
+        }/* else {
             stmt.setString(1, "Idol");
+            stmt.setString(2, username);
+            stmt.setString(3, password);
             stmt.executeQuery(query);
             if (account.next()) {
                 int idolID = account.getInt("idolID");
                 //return new Idol(idolID,)
             }
-        }
-
-         */
-
-
-        User temp = new User(1,"Rithik", "2233293@slu.edu.ph", "rithik", "09177900153", "1");
-        return temp;
+           */
+        return null;
+       // User temp = new User(1,"Rithik", "2233293@slu.edu.ph", "rithik", "09177900153", "1");
+       // return temp;
     }
 
     /**
