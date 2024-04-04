9f40960e326a04a8ad01ab164dd6a78f902eb342
Rithik Tank <2233293@slu.edu.ph>
2024-04-04 14:12:27 +0800

    Idol login

--- src/shared/res/DataPB.java
+++ src/shared/res/DataPB.java
@@ -24,19 +24,19 @@ public class DataPB {
      *  READ THE LINE COMMENTS BEFORE RUNNING THE PROGRAM
      */
     public static void setCon() {
-       try {
+        try {
             // Developer should add the Schema name right after the 3306/
-            String url = "jdbc:mysql://localhost:3306/deans5";
+            String url = "jdbc:mysql://localhost:8889/deans5";
 
             // User and Password should be changed dynamically by the developer
             String user = "root";
             String password = "root";
 
             con = DriverManager.getConnection(url, user, password);
-       }catch (Exception e){
-           System.out.println("Database connection failed");
-           e.printStackTrace();
-       }
+        }catch (Exception e){
+            System.out.println("Database connection failed");
+            e.printStackTrace();
+        }
     }
 
     public static void closeCon(){
@@ -55,12 +55,11 @@ public class DataPB {
      * Method to handle validating user login authentication
      * @return userExists
      */
-    public static Object validateUser(String username, String password) throws SQLException{
+    public static Object validateUser(String username, String password) throws SQLException {
         //TODO : Access schema and check whether user exists
         DataPB.setCon();
 
-        String tableName = "User";
-        String query = "SELECT userID, username, GCashNumber, profilePictureAddress FROM " + tableName + " WHERE username=? and password=?";
+        String query = "SELECT userID, username, GCashNumber, profilePictureAddress FROM User WHERE username=? and password=?";
 
         PreparedStatement stmt = con.prepareStatement(query);
 
@@ -74,19 +73,34 @@ public class DataPB {
             String gCashNumber = account.getString("GCashNumber");
             String ppAddress = account.getString("profilePictureAddress");
             return new User(userID, username, email, password, gCashNumber, ppAddress);
-        }/* else {
-            stmt.setString(1, "Idol");
-            stmt.setString(2, username);
-            stmt.setString(3, password);
-            stmt.executeQuery(query);
+        } else {
+            query = "SELECT idolID, idolName, GCashNumber, idolType, idolStatus, voiceCallRate, " +
+                    "videoCallRate, fbAccount, xAccount, igAccount, bio, quote, profilePictureAddress  FROM Idol WHERE username=? and password=?";
+            stmt = con.prepareStatement(query);
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+            account = stmt.executeQuery();
             if (account.next()) {
+                boolean boolStatus = false;
                 int idolID = account.getInt("idolID");
-                //return new Idol(idolID,)
+                String idolName = account.getString("idolName");
+                String gCashNumber = account.getString("gCashNumber");
+                String type = account.getString("idolType");
+                String status = account.getString("idolStatus");
+                if (status.equalsIgnoreCase("Verified"))
+                    boolStatus = true;
+                double voiceRate = account.getDouble("voiceCallRate");
+                double videoRate = account.getDouble("videoCallRate");
+                String fb = account.getString("fbAccount");
+                String x = account.getString("xAccount");
+                String ig = account.getString("igAccount");
+                String bio = account.getString("bio");
+                String quote = account.getString("quote");
+                String pp = account.getString("profilePictureAddress");
+                return new Idol(idolID, username, idolName, gCashNumber, type, boolStatus, voiceRate, videoRate, fb, x, ig, bio, quote, pp, password);
             }
-           */
-        return null;
-       // User temp = new User(1,"Rithik", "2233293@slu.edu.ph", "rithik", "09177900153", "1");
-       // return temp;
+            return null;
+        }
     }
 
     /**
