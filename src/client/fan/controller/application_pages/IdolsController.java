18fba45c79a3ae7108985a577550f6bb913b5d4a
RAMON EMMIEL JASMIN <2230748@slu.edu.ph>
2024-04-05 21:30:18 +0800

    Initial code for IdolsController constructor

--- src/client/fan/controller/application_pages/IdolsController.java
+++ src/client/fan/controller/application_pages/IdolsController.java
@@ -1,4 +1,28 @@
 package client.fan.controller.application_pages;
 
+import client.fan.model.application_pages.IdolsModel;
+import client.fan.view.application_pages.IdolsView;
+import shared.res.Idol;
+
+import java.sql.SQLException;
+
 public class IdolsController {
+
+    IdolsView view;
+    IdolsModel model;
+
+    public IdolsController(IdolsView view, IdolsModel model) throws SQLException {
+        this.view = view;
+        this.model = model;
+
+        for (Idol idol : model.getAllIdols()){
+            view.getPnlIdolsContainer().add(new IdolsView.IdolDetailsPanel(idol.getProfilePictureAddress(),
+                    idol.getIdolName(),
+                    idol.getIdolType(),
+                    Double.toString(idol.getVideoCallRate()),
+                    Double.toString(idol.getVoiceCallRate())
+                    ));
+        }
+    }
+
 }
